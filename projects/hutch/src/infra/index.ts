74723e378ec4d3708ed83e3ba22f0302bbde4b10
Fayner Brack <github@fagnermartins.com>
2026-04-20 19:56:08 +1000

    fix(hutch): rename readers stat alias to authenticated_unique_readers

--- projects/hutch/src/infra/index.ts
+++ projects/hutch/src/infra/index.ts
@@ -342,7 +342,7 @@ new aws.cloudwatch.Dashboard("readplace-analytics", {
 						"| filter ispresent(visitor_hash)",
 						"| filter is_authenticated",
 						"| filter path like /^\\/[^\\/]+\\/read$/",
-						"| stats count_distinct(visitor_hash) as authenticated_readers by bin(1d)",
+						"| stats count_distinct(visitor_hash) as authenticated_unique_readers by bin(1d)",
 					].join(" "),
 					x: 12, y: 24, width: 12, height: 8,
 					view: "timeSeries",
